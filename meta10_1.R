e7385236fbdd92db7c8b1f49e6033c8a26edbba1
arturlascala <arturlascala@gmail.com>
2020-10-28 19:50:35 -0300

    Create meta10_1.R

---
+++ meta10_1.R
@@ -0,0 +1,58 @@
+pnad_ODS <- function(ano_inicial, tri_inicial){
+    
+    media_maispobres <- function(x){
+        x <- x$variables
+        x <- x %>% filter(RM_RIDE=="Região Metropolitana de São Paulo (SP)")
+        avg <- x %>% filter(VD4020 < quantile(VD4020, na.rm=T, probs = .4)) %>% summarise(mean(VD4020))
+        return(avg)
+    }
+    
+    media_geral <- function(x){
+        x <- x$variables
+        x <- x %>% filter(RM_RIDE=="Região Metropolitana de São Paulo (SP)") # Verificar esse ponto!
+        avg <- x %>% select(VD4020) %>% summarise(mean(VD4020, na.rm = T))
+        return(avg)
+    }
+    
+    if(ano_inicial%%1==0 & ano_inicial<2017){stop("Ano deve ser número inteiro maior ou igual a 2017.")}
+    if(tri_inicial%%1==0 & (tri_inicial<1 | tri_inicial>4)){stop("Trimestre deve ser número inteiro entre 1 e 4")}
+    
+    resultado <- data.frame("ano" = NULL, "trimestre" = NULL, "renda.<40" = NULL, "renda.geral"=NULL)
+    
+    for (i in ano_inicial:lubridate::year(Sys.Date())) {
+            
+            if(i == ano_inicial){
+                for (j in tri_inicial:4) {
+                    try(
+                        x <- PNADcIBGE::get_pnadc(year = i, 
+                                                  quarter = j, 
+                                                  vars = c("RM_RIDE","VD4020"), 
+                                                  deflator = T, 
+                                                  defyear = 2019, 
+                                                  defperiod = 4
+                        ),silent = T
+                    )
+                    try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
+                    if(exists("x")) rm(x)
+                }
+            }
+            
+            else{
+                for(j in 1:4){
+                    try(
+                        x <- PNADcIBGE::get_pnadc(year = i, 
+                                                  quarter = j, 
+                                                  vars = c("RM_RIDE","VD4020"), 
+                                                  deflator = T, 
+                                                  defyear = 2019, 
+                                                  defperiod = 4
+                        ),silent = T
+                    )
+                    try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
+                    if(exists("x")) rm(x)
+                }
+            }
+        }
+    names(resultado) <- c("Ano", "Trimestre", "Média_<40%", "Média_Geral")
+    return(resultado)
+}
