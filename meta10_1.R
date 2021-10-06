fc9459e566f1a1d847fa8e52885cfae16b64b99c
arturlascala <arturlascala@gmail.com>
2021-10-06 13:31:16 -0300

    minor fixes

--- meta10_1.R
+++ meta10_1.R
@@ -12,7 +12,7 @@ pnad_ODS <- function(ano_inicial, tri_inicial){
     
     media_geral <- function(x){
         x <- x$variables
-        x <- x %>% filter(Capital=="Município de São Paulo (SP)") # Verificar esse ponto!
+        x <- x %>% filter(Capital=="Município de São Paulo (SP)")
         avg <- x %>% select(VD4020) %>% summarise(mean(VD4020, na.rm = T))
         return(avg)
     }
@@ -23,39 +23,33 @@ pnad_ODS <- function(ano_inicial, tri_inicial){
     resultado <- data.frame("ano" = NULL, "trimestre" = NULL, "renda.<40" = NULL, "renda.geral"=NULL)
     
     for (i in ano_inicial:lubridate::year(Sys.Date())) {
-            
-            if(i == ano_inicial){
-                for (j in tri_inicial:4) {
-                    try(
-                        x <- PNADcIBGE::get_pnadc(year = i, 
-                                                  quarter = j, 
-                                                  vars = c("Capital","VD4020"), 
-                                                  deflator = T, 
-                                                  defyear = 2019, 
-                                                  defperiod = 4
-                        ),silent = T
-                    )
-                    try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
-                    if(exists("x")) rm(x)
-                }
-            }
-            
-            else{
-                for(j in 1:4){
-                    try(
-                        x <- PNADcIBGE::get_pnadc(year = i, 
-                                                  quarter = j, 
-                                                  vars = c("Capital","VD4020"), 
-                                                  deflator = T, 
-                                                  defyear = 2019, 
-                                                  defperiod = 4
-                        ),silent = T
-                    )
-                    try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
-                    if(exists("x")) rm(x)
-                }
+        
+        if(i == ano_inicial){
+            for (j in tri_inicial:4) {
+                try(
+                    x <- PNADcIBGE::get_pnadc(year = i, 
+                                              quarter = j, 
+                                              vars = c("Capital","VD4020")
+                    ),silent = T
+                )
+                try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
+                if(exists("x")) rm(x)
             }
         }
+        
+        else{
+            for(j in 1:4){
+                try(
+                    x <- PNADcIBGE::get_pnadc(year = i, 
+                                              quarter = j, 
+                                              vars = c("Capital","VD4020")
+                    ),silent = T
+                )
+                try(resultado <- rbind(resultado, list(i,j,media_maispobres(x),media_geral(x))),silent = T)
+                if(exists("x")) rm(x)
+            }
+        }
+    }
     names(resultado) <- c("Ano", "Trimestre", "Média_<40%", "Média_Geral")
     return(resultado)
 }
